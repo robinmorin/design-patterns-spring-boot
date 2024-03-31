7d24435bc3da7216e6337de4b17793c14c7aa40d
Robin Bray Morin <robinmorin79@gmail.com>
2024-03-31 00:35:37 -0300

    Adding project SimpleMemStorage
    - Project for manages the Storage Data in Memory for a few data to practice purposes.

---
+++ simple-mem-storage/src/test/java/com/personal/simplememstorage/SimpleMemStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.simplememstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMemStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
