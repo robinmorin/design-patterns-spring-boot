d107e2481d100e6cbb5d2b618216eebeee641854
Robin Bray Morin <robinmorin79@gmail.com>
2024-04-07 18:17:56 -0300

    First Commit

---
+++ abstract-factory-pattern/src/test/java/com/personal/pattern/abstractfactory/AbstractFactoryPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.pattern.abstractfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractFactoryPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
